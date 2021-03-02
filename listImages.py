90638513920c38d2509a5746a22211e2a4cdf435
Camden Kronhaus <kronhaus@bu.edu>
2021-03-01 19:51:58 -0500

    list all official docker images

---
+++ listImages.py
@@ -0,0 +1,31 @@
+import os
+
+
+def printToFile(my_set):
+    file = open("image_list.txt", "w")
+    for val in my_set:
+        file.write(val + "\n")
+    file.close()
+
+def generateList():
+     # search through all combination of paris of a->z (duplicate searches do not affect results)
+    # initialize new set
+    my_set = {}
+    my_set = set()
+    for i in range(ord('a'), ord('z')+1):
+        for j in range(ord('a'), ord('z')+1):
+            # print("current search: " + chr(i) + chr(j))
+            search_chars = chr(i) + chr(j)
+            # command to search, read into "data"
+            data = os.popen(
+                "docker search --filter is-official=true --format \"{{.Name}}\" " + search_chars).read()
+            data = data.split()
+            # add new unique elements to set
+            my_set.update(data)
+            if len(my_set) >= 163:
+                printToFile(my_set)
+                return
+
+if __name__ == '__main__':
+    generateList()
+
