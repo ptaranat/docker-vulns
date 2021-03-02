da52f946366241214df6669d9f1e04fc93be2969
Camden Kronhaus <kronhaus@bu.edu>
2021-03-02 15:06:48 -0500

    output dir

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import json
+import os
+from pprint import pprint
+from termcolor import cprint
+
 
 # with open("scan_result") as f:
 #     data = json.load(f)
@@ -11,8 +15,20 @@ import json
 #     data = json.load(f)
 #     HR_scan_results_file.write(json.dumps(data, indent=1))
 
-with open("scan_results.out") as f:
+with open("HR_scan_results.out.out") as f:
     data = json.load(f)
     # print(json.dumps(data, indent=1))
-    print(json.dumps(data["archlinux"]["matches"][0]["vulnerability"]["id"]))
+    print(json.dumps(data["archlinux"]))
 
+# results = {}
+
+# try:
+#     data = os.popen(
+#     "grype glassfish -o json").read()
+#     results['hello'] = json.loads(data)
+#     #remove the image
+# except:
+#     cprint("Exception on: glassfish", 'grey', 'on_red')
+#     pass
+
+# print(results)
