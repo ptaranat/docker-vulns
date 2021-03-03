093dbe93dd8260bce7ecdcfee7ad537cd5a7a437
Panat Taranat <panat.taranat@gmail.com>
2021-03-02 19:21:09 -0500

    Add function to count critical CVEs

--- parseCVE.py
+++ parseCVE.py
@@ -3,8 +3,8 @@ import json
 import csv
 from collections import Counter
 
-def countCVE():
-    with open("backup/scan_results.json", "r") as f:
+def countCVE(filename):
+    with open(filename, "r") as f:
         data = json.loads(f.read())
         keys = data.keys()
         print("image,num_vulns,")
@@ -13,26 +13,50 @@ def countCVE():
             print(f"{i},{count},")
 
 
-def countPackages():
-    with open("backup/scan_results.json", "r") as f:
+def countPackages(filename):
+    with open(filename, "r") as f:
         data = json.loads(f.read())
         keys = data.keys()
-        pkg_count = Counter()
+        c = Counter()
 
         for i in keys:
             matches = data[i]["matches"]
             for m in matches:
-                pkg_count.update({m["artifact"]["name"] : 1})
+                c.update({m["artifact"]["name"] : 1})
         
         with open("pkgcount.csv", "w") as csvfile:
             fieldnames=["Package", "Count"]
             writer = csv.writer(csvfile)
             writer.writerow(fieldnames)
-            for key, value in pkg_count.items():
+            for key, value in c.items():
+                writer.writerow([key]+[value])
+
+def countCritCVE(filename):
+    with open(filename, "r") as f:
+        data = json.loads(f.read())
+        keys = data.keys()
+        c = Counter()
+
+        for i in keys:
+            matches = data[i]["matches"]
+            for m in matches:
+                if m["vulnerability"]["severity"] == "High":
+                    c.update({m["vulnerability"]["id"] : 1})
+
+        with open("critcount.csv", "w") as csvfile:
+            fieldnames=["CVE", "Count"]
+            writer = csv.writer(csvfile)
+            writer.writerow(fieldnames)
+            for key, value in c.items():
                 writer.writerow([key]+[value])
 
 
 
+
+
+
 if __name__ == '__main__':
-    # countCVE()
-    countPackages()
+    filename = "backup/scan_results.json"
+    # countCVE(filename)
+    # countPackages(filename)
+    countCritCVE(filename)
